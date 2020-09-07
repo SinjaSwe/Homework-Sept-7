4d05f211d2c8ed460600f112edc459b8e01977ff
Sinead Jacobsson <sinead.jacobsson@gmail.com>
2020-09-07 14:14:33 +0200

    Ternary operator

--- Homework Sept 7/Program.cs
+++ Homework Sept 7/Program.cs
@@ -9,19 +9,22 @@ namespace Homework_Sept_7
             Console.WriteLine("Hello World!");
 
             Console.WriteLine("How old are you?");
-                          
-            Console.ReadKey();
 
             int numberOne = Convert.ToInt32(Console.ReadLine());
             
-            int response = (numberOne > 20)     ? "You are allowed to drink beer" 
-                                                : "You are not allowed to drink beer";
+            string response = (numberOne > 20)      ? "You are allowed to drink beer" 
+                                                    : "You are not allowed to drink beer! Welcome to the club!";  
 
-            
+            Console.WriteLine(response);
+            Console.ReadKey();
 
-          
+
+
         }
+
     }
+}
+
 
 
 
